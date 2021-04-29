b58815b09bf31a1dfd69920286d599de22046637
Ian Darland <iansmbpro@ians-mbp.lan>
2021-04-29 09:58:42 -0500

    trouble shooting put functions working now

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -47,7 +47,7 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try{
-    const updatedCategory = Category.update(req.body, {
+    const updatedCategory = await Category.update(req.body, {
       where: {
         id: req.params.id,
       },
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -47,7 +47,7 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try{
-    const updatedTag = Tag.update(req.body, {
+    const updatedTag = await Tag.update(req.body, {
       where: {
         id: req.params.id,
       },
